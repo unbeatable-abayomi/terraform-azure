99b5b9b0d65e576dcac58facac6dedda2250c0b3
unbeatable-abayomi <igwubor@outlook.com>
2026-04-03 18:34:44 +0100

    Added gitignore file

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "terraform-rg"
   location = "East US"
-}
+} 
 
 # Output the resource group name after creation
 output "resource_group_name" {
