31e3aa2d98c509d046f5729101a296aab814df33
unbeatable-abayomi <igwubor@outlook.com>
2026-04-03 19:06:42 +0100

    Added storage Account

--- main.tf
+++ main.tf
@@ -9,6 +9,20 @@ resource "azurerm_resource_group" "example" {
   location = "East US"
 } 
 
+# Create  a Storage Account
+resource "azurerm_storage_account" "example" {
+  name                     = "terraformstorage125yomi"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Output the strorage account name after creation
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
+
 # Output the resource group name after creation
 output "resource_group_name" {
   value = azurerm_resource_group.example.name
