25f648c067003af6d807f0ac134ea667709ad4ad
unbeatable-abayomi <igwubor@outlook.com>
2026-04-03 06:14:54 +0100

    Added/Updated Main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Specify the Terraform provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "terraform-rg"
+  location = "East US"
+}
+
+# Output the resource group name after creation
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
