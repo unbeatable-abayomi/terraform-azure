7252920a20977d8bc8eed7bbf21b4052b8b55c15
unbeatable-abayomi <igwubor@outlook.com>
2026-04-05 00:39:18 +0100

    Adding Values to AWS main.tf

--- aws-vm/main.tf
+++ aws-vm/main.tf
@@ -65,8 +65,8 @@ resource "aws_instance" "web" {
 
   #instance_type = "t2.micro"             # Free Tier eligible
   instance_type = var.instance_type_name
-  #key_name      = "us-east-1-devops-user" # The exact name as it appears in AWS Console
-  key_name      = var.key_pair_name
+  key_name      = "us-east-1-devops-user" # The exact name as it appears in AWS Console
+  #key_name      = var.key_pair_name
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
