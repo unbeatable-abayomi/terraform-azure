3554c1d4a5b1e8f7504ac2581dd1d343eccad953
unbeatable-abayomi <igwubor@outlook.com>
2026-04-05 00:40:37 +0100

    Adding Values to Azure main.tf

--- azure-vm/main.tf
+++ azure-vm/main.tf
@@ -97,11 +97,30 @@ resource "azurerm_linux_virtual_machine" "example" {
 }
 
 
+# resource "azurerm_network_security_group" "example" {
+#   name                = "example-nsg"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+
+#   security_rule {
+#     name                       = "SSH"
+#     priority                   = 1001
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "22"
+#     source_address_prefix      = "*" # In a real NOC role, you would change this to your home IP
+#     destination_address_prefix = "*"
+#   }
+# }
+
 resource "azurerm_network_security_group" "example" {
   name                = "example-nsg"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
+  # Existing SSH Rule
   security_rule {
     name                       = "SSH"
     priority                   = 1001
@@ -110,7 +129,19 @@ resource "azurerm_network_security_group" "example" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*" # In a real NOC role, you would change this to your home IP
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  # HTTP Rule
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
